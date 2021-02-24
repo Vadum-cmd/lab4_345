3558f8ef3dd670b105aa87c83493777c6c6fc8bc
Treskot Vadym <vadym.treskot@ucu.edu.ua>
2021-02-24 22:56:28 +0300

    forgot theese

--- classroom.py
+++ classroom.py
@@ -1,2 +1,22 @@
 class Classroom:
-    pass
+    '''
+    Makes a class for work with data about classrooms.
+    '''
+    def __init__(self, number, capacity, equipment):
+        self.number = number
+        self.capacity = capacity
+        self.equipment = equipment
+
+
+    def __str__(self):
+        return """Classroom {0} has a capacity of {1} persons and has the following \
+equipment: {2}.""".format(self.number, self.capacity, ", ".join(self.equipment))
+
+
+    def is_larger(self, room):
+        pass
+
+
+classroom_016 = Classroom('016', 80, ['PC', 'projector', 'mic'])
+classroom_007 = Classroom('007', 12, ['TV'])
+classroom_016.is_larger(classroom_007)
--- point.py
+++ point.py
@@ -1,2 +1,15 @@
 class Point:
-    pass
+    """
+    Makes class to work with coordinates of the point.
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def distance_to_origin(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+
+if __name__ == "__main__":
+    print("I'm main!")
