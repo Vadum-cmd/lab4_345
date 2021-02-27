9c00b3bc86eca0443c5ea7c6fb28b5aa88c203a0
Vadum-cmd <74098778+Vadum-cmd@users.noreply.github.com>
2021-02-27 23:37:40 +0200

    Add files via upload

--- classroom.py
+++ classroom.py
@@ -13,10 +13,32 @@ class Classroom:
 equipment: {2}.""".format(self.number, self.capacity, ", ".join(self.equipment))
 
 
-    def is_larger(self, room):
-        pass
+    def is_larger(self, oth_room):
+        """
+        Gives an answer about which room can accommodate more people.
+        """
+        return self.capacity > oth_room.capacity
+    
+
+    def equipment_differences(self, oth_room):
+        """
+        Compares equipment amoung rooms.
+        """
+        unique_equipment = []
+        for thing in self.equipment:
+            if thing not in oth_room.equipment:
+                unique_equipment.append(thing)
+        return unique_equipment
+    
+
+    def __repr__(self):
+        return f'Classroom({self.number}, {self.capacity}, {self.equipment})'
 
 
-classroom_016 = Classroom('016', 80, ['PC', 'projector', 'mic'])
-classroom_007 = Classroom('007', 12, ['TV'])
-classroom_016.is_larger(classroom_007)
+if __name__ == "__main__":
+    classroom_016 = Classroom('016', 80, ['PC', 'projector', 'mic'])
+    classroom_007 = Classroom('007', 12, ['TV'])
+    # print(classroom_016)
+    # print(classroom_016.is_larger(classroom_007))
+    # print(classroom_016.equipment_differences(classroom_007))
+    # print(classroom_016.__repr__())
